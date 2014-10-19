adab4af090500c41d872b2d299794772d33b2459
Antonio Nalesso <nbit001@gmail.com>
2014-10-19 23:06:57 +0100

    AN: Initial Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+
+
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/js", express.static(__dirname + "/js"));
+
+app.get("/", function(req, res) {
+    res.sendfile(__dirname + "/views/index.html");
+});
+
+io.on("connection", function(socket) {
+    socket.on("Pi", function(data) {
+      io.emit("Pi", data);
+    });
+});
+
+server.listen((process.env.PORT || 1234), function() {
+    console.log("App started! on: localhost:1234")
+});
